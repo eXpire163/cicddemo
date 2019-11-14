4cc8d2a5551086bb9ae3560ac436a588bc496e4e
eXpire163 <git@mms2k.de>
2019-11-14 21:05:16 +0100

    first commit

---
+++ src/main/java/org/hellofheaven/cicddemo/CicddemoApplication.java
@@ -0,0 +1,13 @@
+package org.hellofheaven.cicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicddemoApplication.class, args);
+	}
+
+}
